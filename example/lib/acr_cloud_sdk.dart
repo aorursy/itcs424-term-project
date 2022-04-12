f59be7b0c6bd26bb946956cd34997ff63ecabc58
gailtasha <Tasha.set@student.mahidol.ac.th>
2022-04-13 01:22:01 +0700

    add shazamm feature

---
+++ example/lib/acr_cloud_sdk.dart
@@ -0,0 +1,4 @@
+library acr_cloud_sdk;
+
+export './song_model.dart';
+export './acr_cloud.dart';
---
+++ example/lib/views/signin_page.dart
@@ -0,0 +1,81 @@
+import 'package:acr_cloud_sdk_example/reusable_widgets/reusable_widgets.dart';
+import 'package:acr_cloud_sdk_example/utils/color_utils.dart';
+import 'package:acr_cloud_sdk_example/views/home_page.dart';
+import 'package:acr_cloud_sdk_example/views/signup_page.dart';
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  TextEditingController _passwordEditingController = TextEditingController();
+  TextEditingController _emailEditingController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          hexStringToColor("CB2B93"),
+          hexStringToColor("9546C4"),
+          hexStringToColor("5E61F4")
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(
+                20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+            child: Column(
+              children: <Widget>[
+                logoWidget("assets/images/logo.png"),
+                SizedBox(
+                  height: 30,
+                ),
+                reusableTextField("Username", Icons.person_outline, false,
+                    _emailEditingController),
+                SizedBox(
+                  height: 20,
+                ),
+                reusableTextField("Password", Icons.lock_outline, true,
+                    _passwordEditingController),
+                SizedBox(
+                  height: 20,
+                ),
+                signInSignUpButton(context, true, () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HomePage()));
+                }),
+                signUpOption()
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row signUpOption() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Don't have an account?",
+            style: TextStyle(color: Colors.white70)),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => SignUpPage()));
+          },
+          child: const Text(
+            " Sign Up",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ],
+    );
+  }
+}
